75a2f2fdfc688f1de63b3c8428fc567e26e270ad
Jordan.Young <jordan.young@glu.com>
2015-02-20 13:37:49 -0800

    modified to output on tick tuple

--- src/main/java/helloworld/LearningStormGlobalAggBolt.java
+++ src/main/java/helloworld/LearningStormGlobalAggBolt.java
@@ -9,13 +9,11 @@ import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Fields;
-import backtype.storm.tuple.Values;
-import storm.starter.util.TupleHelpers;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class LearningStormAggBolt extends BaseRichBolt {
+public class LearningStormGlobalAggBolt extends BaseRichBolt {
 
     private static final long serialVersionUID = 1L;
     private OutputCollector collector;
@@ -35,30 +33,18 @@ public class LearningStormAggBolt extends BaseRichBolt {
     @Override
     public void execute(Tuple input) {
         // Get the field "site" from input tuple.
-
-        if (TupleHelpers.isTickTuple(input)) {
-            for (String key: outputMap.keySet()) {
-                collector.emit(new Values(key, outputMap.get(key)));
-                System.out.println("Total Counts: " + outputMap.toString());
-            }
-            outputMap.clear();
+        String test = input.getStringByField("site");
+        if (outputMap.containsKey(test)) {
+            outputMap.put(test, outputMap.get(test) + 1);
         }
         else {
-            String test = input.getStringByField("site");
-            if (outputMap.containsKey(test)) {
-                outputMap.put(test, outputMap.get(test) + 1);
-            }
-            else {
-                outputMap.put(test,1);
-            }
+            outputMap.put(test,1);
         }
-	    collector.ack(input);
-   //     countMetric.incr();
-
+        collector.ack(input);
+        //     countMetric.incr();
+        System.out.println("Total Counts: " + outputMap.toString());
     }
 
-    @Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
-        declarer.declare(new Fields("msgType", "msg"));
     }
 }
